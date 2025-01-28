cbd64386acbeb5cd36b2c7fdcc2da1ae955cd40c
AzureAD\佐藤有衣 <yu-satoseassist>
2025-01-28 10:12:33 +0900

    FizzBuzz

--- src/main/java/com/example/demo/fizzBuzz/controller/FizzBuzzController.java
+++ src/main/java/com/example/demo/fizzBuzz/controller/FizzBuzzController.java
@@ -1,26 +1,31 @@
 package com.example.demo.fizzBuzz.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.fizzBuzz.service.FizzBuzzService;
 
 //リクエストの受け取り
-
+@Controller
 //fizzBuzz.htmlに掛け算結果を渡してあげる、fizzBuzz.htmlの表示
 public class FizzBuzzController {
 	private final FizzBuzzService fizzBuzzService;
-	//依存性の注入
-		//Serviceクラスのインスタンスをspringbootから受け取る
-public final FizzBuzzController	(FizzBuzzService fizzBuzzService) {
-	this.fizzBuzzService=fizzBuzzService;
-}
-@GetMapping("fizzBuzz")
-public String fizzBuzz(Model model) {
-	int result =fizzBuzzService;
-	//リクエストスコープに格納
-	model.addAttribute("result", result);
-	return "fizzbuzz.html";
 
-}
+	//依存性の注入
+	//Serviceクラスのインスタンスをspringbootから受け取る
+	public FizzBuzzController(FizzBuzzService fizzBuzzService) {
+		this.fizzBuzzService = fizzBuzzService;
+	}
+
+	@GetMapping("fizzBuzz")
+	public String fizzBuzz(Model model) {
+		List<String> result = fizzBuzzService.getFizzBuzzList();
+		//リクエストスコープに格納
+		model.addAttribute("result", result);
+		return "fizzBuzz.html";
+
+	}
 }
--- src/main/java/com/example/demo/fizzBuzz/service/FizzBuzzService.java
+++ src/main/java/com/example/demo/fizzBuzz/service/FizzBuzzService.java
@@ -3,23 +3,30 @@ package com.example.demo.fizzBuzz.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+//リクエストの受け取り
+@Service
 public class FizzBuzzService {
+	public List<String> getFizzBuzzList() {
 	//計算処理を行うメソッドを定義するクラス
 	//リストはList<データ型> 変数名 = new ArrayList<>()でできます。
-	public List<String> fizzBuzz=new ArrayList<>();
+	List<String> fizzBuzzList=new ArrayList<String>();
+	
 	//1から100までの数字を表示
 	for(int i = 1;i<=100;i++)
 	{
 		//3と5のどちらでも割り切れる数を求めてFizzBuzzと表示させる。
 		if (i % 3 == 0 && i % 5 == 0) {
-			fizzBuzz.add("FizzBuzz");
+			 fizzBuzzList.add("FizzBuzz");
 
 		} else if (i % 3 == 0) {
-			fizzBuzz.add("Fizz");
+			 fizzBuzzList.add("Fizz");
 		} else if (i % 5 == 0) {
-			fizzBuzz.add("Buzz");
+			 fizzBuzzList.add("Buzz");
+		}else {
+			fizzBuzzList.add(String.valueOf(i));
 		}
 	}
-	return fizzBuzz;
+	return  fizzBuzzList;
 }
 }
